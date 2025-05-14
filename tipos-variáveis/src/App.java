2fa8b605dbf5a355fcfd09c1d21aba7a7a3c6a69
joaovfb20 <joaovitorfbarbosa@gmail.com>
2025-05-14 17:34:08 -0300

    doubleConstante

--- tipos-variáveis/src/App.java
+++ tipos-variáveis/src/App.java
@@ -5,5 +5,9 @@ public class App {
         numero = 10;
         System.out.println("O valor de numero é: " + numero);
 
+        final double VALOR_PI = 3.14;
+
+        System.out.println("O valor de PI é: " + VALOR_PI);
+
     }
 }
