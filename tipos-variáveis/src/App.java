32ad9c357d171bad213262426a291d74a8298313
joaovfb20 <joaovitorfbarbosa@gmail.com>
2025-05-14 17:31:00 -0300

    app

---
+++ tipos-variáveis/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int numero = 5;
+
+        numero = 10;
+        System.out.println("O valor de numero é: " + numero);
+
+    }
+}
