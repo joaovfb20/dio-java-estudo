1723531170405aaea75a18702fd5eb593a8d82ee
joaovfb20 <joaovitorfbarbosa@gmail.com>
2025-05-14 17:55:27 -0300

    OperadoresRelacionais

---
+++ tipos-variáveis/src/OperadoresRelacionais.java
@@ -0,0 +1,14 @@
+public class OperadoresRelacionais {
+    public static void main(String[] args) {
+        // Operadores Relacionais
+        int a = 10;
+        int b = 5;
+
+        System.out.println("Igualdade: " + (a == b)); // false
+        System.out.println("Diferença: " + (a != b)); // true
+        System.out.println("Maior que: " + (a > b)); // true
+        System.out.println("Menor que: " + (a < b)); // false
+        System.out.println("Maior ou igual a: " + (a >= b)); // true
+        System.out.println("Menor ou igual a: " + (a <= b)); // false
+    }
+}
