8a9374969a97aec8148567a70f1b233303a39d58
joaovfb20 <joaovitorfbarbosa@gmail.com>
2025-05-14 17:47:46 -0300

    boolean

---
+++ tipos-variáveis/src/Operadores2.java
@@ -0,0 +1,14 @@
+public class Operadores2 {
+    public static void main(String[] args) {
+        int numero = 5;
+
+        numero++;
+
+        System.out.println("O valor de numero é: " + ++ numero);
+        // Operadores de Atribuição
+
+        boolean resultado = true;
+        resultado = !resultado;
+        System.out.println("O valor de resultado é: " + resultado);
+    }
+}
